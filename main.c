c04f7c214224d65f019a6e7cb45c390126b4fc14
Bruno-Delourme <delourmebruno@gmail.com>
2024-06-24 17:22:50 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    const char BLUE = "BLUE";
+    const char RED = "RED";
+    const char YELLOW = "YELLOW";
+    const char GREEN = "GREEN";
+    const char ORANGE = "ORANGE";
+    char proposition_user = [];
+
+    printf("Proposez une composition de 4 couleurs!\n");
+    scanf("%c %c %c %c")
+
+    return 0;
+}
