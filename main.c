6eeb4fad1fddffd26692a92bfea372ee27ef5212
Bruno-Delourme <delourmebruno@gmail.com>
2024-06-25 17:24:00 +0200

    reading tries of user, init counter

--- main.c
+++ main.c
@@ -3,15 +3,34 @@
 
 int main()
 {
-    const char BLUE = "BLUE";
-    const char RED = "RED";
-    const char YELLOW = "YELLOW";
-    const char GREEN = "GREEN";
-    const char ORANGE = "ORANGE";
-    char proposition_user = [];
+    const int MAX_TRY = 10;
+    const int MAX_COLORS = 5;
+    const char COLORS[] = {'BLUE', 'RED', 'YELLOW', 'GREEN', 'ORANGE'};
+    const int CODE_SIZE = 4;
 
-    printf("Proposez une composition de 4 couleurs!\n");
-    scanf("%c %c %c %c")
+    char code_secret[CODE_SIZE] = {'BLUE', 'RED', 'YELLOW', 'GREEN'};
+    char user_propose[CODE_SIZE] = void;
+
+    for(int user_propose = 0; user_propose<= MAX_TRY +1; user_propose++)
+    {
+        //lecture saisie utilisateur
+    printf("Propose une combinaison de 4 couleurs avec {'BLUE', 'RED', 'YELLOW', 'GREEN', 'ORANGE'}");
+    fflush(stdin);
+    scanf("%c%c%c%c", user_propose, (user_propose+1),(user_propose+2),(user_propose+3));
+
+
+        while(user_propose<= MAX_TRY);
+        continue;
+        printf("> TENTATIVE N*%d/%d\n", user_propose, MAX_TRY);
+        if(user_propose= MAX_TRY);
+        printf("> TENTATIVE N*%d/%d FIN\n", user_propose, MAX_TRY);
+        break;
+
+    }
+
+
+
+
 
     return 0;
 }
