a8ffb0fe5665554e66d33537e2b9c7afb040db38
Bruno-Delourme <delourmebruno@gmail.com>
2024-06-25 18:17:44 +0200

    return good position on colors

--- main.c
+++ main.c
@@ -3,47 +3,54 @@
 
 int main()
 {
-    const int MAX_TRY = 10;
-    const int MAX_COLORS = 5;
-    const char COLORS[] = {'B', 'R', 'Y','G' 'O'};
-    const int CODE_SIZE = 4;
+#define MAX_TRY 5
+#define MAX_COLORS 5
+#define CODE_SIZE 4
 
-    char code_secret[CODE_SIZE] = {'B', 'R', 'Y','G'};
-    char user_propose[CODE_SIZE] = void;
+const char COLORS[] = {'B', 'R', 'Y', 'G', 'O'};
 
-    for(int user_propose = 0; user_propose<= MAX_TRY +1; user_propose++)
+    char code_secret[] = {'B', 'R', 'Y', 'G'};
+    char user_propose[CODE_SIZE];
+
+    for(int nb_try = 1; nb_try<= MAX_TRY; nb_try++)
     {
         //lecture saisie utilisateur
-    printf("Propose une combinaison de 4 couleurs avec {'BLUE', 'RED', 'YELLOW', 'GREEN', 'ORANGE'}");
+    printf("Propose une combinaison de 4 couleurs avec {'B', 'R', 'Y', 'G', 'O'}\n");
     fflush(stdin);
     scanf("%c%c%c%c", user_propose, (user_propose+1),(user_propose+2),(user_propose+3));
 
         //init des compteurs
-        int nb_colors_place = 0;
+        int nb_colors_good_position = 0;
+        int nb_colors_bad_position=0;
 
         // comparaison du code secret et de l'utilisateur
-        for(i=0; i<CODE_SIZE; i++)
+        for(int i=0; i<CODE_SIZE; i++)
         {
             if(code_secret[i] ==  user_propose[i])
             {
-                nb_colors_place++;
+                nb_colors_good_position++;
             }
-
         }
 
 
-        while(user_propose<= MAX_TRY);
-        continue;
-        printf("> TENTATIVE N*%d/%d\n", user_propose, MAX_TRY);
-        if(user_propose= MAX_TRY);
-        printf("> TENTATIVE N*%d/%d FIN\n", user_propose, MAX_TRY);
-        break;
+        //gestion fin de partie
+        if(nb_colors_good_position == CODE_SIZE)
+        {
+            printf("Bravo you won!\n");
+            break;
+        }
+        else if(nb_colors_good_position>= MAX_TRY)
+        {
+            printf("Sadly, you reach the max try!\n");
+            printf("The code was %c%c%c%c", code_secret[0], code_secret[1], code_secret[2], code_secret[3]);
+        }
+        else
+        {
+            printf("\t TRY No%d/%d\n", nb_try, MAX_TRY);
+            printf("Couleurs bien placees : %d\n\n", nb_colors_good_position);
+        }
+
 
     }
-
-
-
-
-
     return 0;
 }
